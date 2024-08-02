d6ce58b4f5ad10770c9b6ffd2f7f2e5d12f7f807
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-08-02 10:23:16 +0400

    created first page for introduction added pic added sass, installed axios downloaded pictures for menuBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import Menu from './containers/Menu.tsx';
+
 
 const App = () => {
-  return <></>;
+  return(
+    <>
+      <Menu/>
+    </>
+  );
 };
 
 export default App;
---
+++ src/containers/Menu.tsx
@@ -0,0 +1,29 @@
+import "./Menu.scss";
+import mainPic from "../img/mainPageAdd.svg"
+import { Button } from 'antd';
+import { RightOutlined } from '@ant-design/icons';
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <div className="menu-starter">
+        <div
+          style={{width:'320px', height:'320px'}}
+          className="bg-white rounded-circle d-flex align-items-center justify-content-center w-100 mb-3">
+          <img
+            style={{ width: '321px', height: '280px' }}
+            src={mainPic} alt="mainPageImg" />
+        </div>
+        <h4 className="mb-5">
+          Enjoy <br/>
+          Your Food
+        </h4>
+        <Button type="dashed" >
+          Get Started <RightOutlined />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
